611054fb828821818149f4520b1aeea64b5d938e
mj <mj@Marks-Mac-mini.local>
2021-07-07 15:20:43 -0600

    Changed all child controls from public properties to private data members

--- TrueCraft.Launcher/Views/MainMenuView.cs
+++ TrueCraft.Launcher/Views/MainMenuView.cs
@@ -5,50 +5,50 @@ namespace TrueCraft.Launcher.Views
 {
     public class MainMenuView : VBox
     {
-        public LauncherWindow Window { get; set; }
+        private LauncherWindow _launcher { get; set; }
 
-        public Label WelcomeText { get; set; }
-        public Button SingleplayerButton { get; set; }
-        public Button MultiplayerButton { get; set; }
-        public Button OptionsButton { get; set; }
-        public Button QuitButton { get; set; }
+        private Label _welcomeText;
+        private Button _singleplayerButton;
+        private Button _multiplayerButton;
+        private Button _optionsButton;
+        private Button _quitButton;
 
         public MainMenuView(LauncherWindow window)
         {
-            Window = window;
+            _launcher = window;
             this.SetSizeRequest(250, -1);
 
-            WelcomeText = new Label("Welcome, " + Window.User.Username)
+            _welcomeText = new Label("Welcome, " + _launcher.User.Username)
             {
                Justify = Justification.Center
             };
-            SingleplayerButton = new Button("Singleplayer");
-            MultiplayerButton = new Button("Multiplayer");
-            OptionsButton = new Button("Options");
-            QuitButton = new Button("Quit Game");
+            _singleplayerButton = new Button("Singleplayer");
+            _multiplayerButton = new Button("Multiplayer");
+            _optionsButton = new Button("Options");
+            _quitButton = new Button("Quit Game");
 
-            SingleplayerButton.Clicked += (sender, e) =>
+            _singleplayerButton.Clicked += (sender, e) =>
             {
-                Window.InteractionBox.Remove(this);
-                Window.InteractionBox.PackEnd(Window.SingleplayerView, true, false, 0);
+                _launcher.InteractionBox.Remove(this);
+                _launcher.InteractionBox.PackEnd(_launcher.SingleplayerView, true, false, 0);
             };
-            MultiplayerButton.Clicked += (sender, e) =>
+            _multiplayerButton.Clicked += (sender, e) =>
             {
-                Window.InteractionBox.Remove(this);
-                Window.InteractionBox.PackEnd(Window.MultiplayerView, true, false, 0);
+                _launcher.InteractionBox.Remove(this);
+                _launcher.InteractionBox.PackEnd(_launcher.MultiplayerView, true, false, 0);
             };
-            OptionsButton.Clicked += (sender, e) =>
+            _optionsButton.Clicked += (sender, e) =>
             {
-                Window.InteractionBox.Remove(this);
-                window.InteractionBox.PackEnd(Window.OptionView, true, false, 0);
+                _launcher.InteractionBox.Remove(this);
+                window.InteractionBox.PackEnd(_launcher.OptionView, true, false, 0);
             };
-            QuitButton.Clicked += (sender, e) => Application.Quit();
+            _quitButton.Clicked += (sender, e) => Application.Quit();
 
-            this.PackStart(WelcomeText, true, false, 0);
-            this.PackStart(SingleplayerButton, true, false, 0);
-            this.PackStart(MultiplayerButton, true, false, 0);
-            this.PackStart(OptionsButton, true, false, 0);
-            this.PackEnd(QuitButton, true, false, 0);
+            this.PackStart(_welcomeText, true, false, 0);
+            this.PackStart(_singleplayerButton, true, false, 0);
+            this.PackStart(_multiplayerButton, true, false, 0);
+            this.PackStart(_optionsButton, true, false, 0);
+            this.PackEnd(_quitButton, true, false, 0);
         }
     }
 }
