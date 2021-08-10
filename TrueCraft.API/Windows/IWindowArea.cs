abff62c4d04e0af58a2e3f6c5d1289fe626b7ffa
mj <mj@Marks-Mac-mini.local>
2021-08-10 15:18:21 -0600

    Documenting members of IWindowArea interface

--- TrueCraft.API/Windows/IWindowArea.cs
+++ TrueCraft.API/Windows/IWindowArea.cs
@@ -2,16 +2,61 @@
 
 namespace TrueCraft.API.Windows
 {
+    /// <summary>
+    /// Represents a collection of related item slots.
+    /// </summary>
+    /// <remarks>
+    /// Note that this does not implement a GUI.
+    /// It represents a data storage that supports a window area on the GUI.
+    /// </remarks>
     public interface IWindowArea : IDisposable
     {
         event EventHandler<WindowChangeEventArgs> WindowChange;
 
+        /// <summary>
+        /// Gets the index (within the parent Window) of the first slot
+        /// in this Window Area.
+        /// </summary>
+        /// <remarks>
+        /// The parent Window contains a collection of Window Area objects.
+        /// The parent Window has an indexer that indexes all Window Area
+        /// objects within itself.  This StartIndex property indicates
+        /// where this Window Area lies within the parent Window's indices.
+        /// </remarks>
         int StartIndex { get; }
+
+        /// <summary>
+        /// Gets the total number of slots within the Window Area.
+        /// </summary>
+        /// <remarks>
+        /// This is usually, but not always, the product of the
+        /// Length and Width.
+        /// </remarks>
         int Length { get; }
+
+        /// <summary>
+        /// Gets the width of the rectangular portion of the Window Area
+        /// in slots.
+        /// </summary>
         int Width { get; }
+
+        /// <summary>
+        /// Gets the height of the rectangular portion of the Window Area
+        /// in slots.
+        /// </summary>
         int Height { get; }
+
+        /// <summary>
+        /// Gets the Array containing the ItemStack objects in this Window Area.
+        /// </summary>
         ItemStack[] Items { get; }
 
+        /// <summary>
+        /// Gets or sets the ItemStack at the given index within this Window Area.
+        /// </summary>
+        /// <param name="index">The index within this Window Area.  The first
+        /// ItemStack is at index 0, not StartIndex.</param>
+        /// <returns></returns>
         ItemStack this[int index] { get; set; }
 
         void CopyTo(IWindowArea area);
