6ca74bf04b3ce3fe17424cf32ebaeac1f2fe12aa
mj <mj@Marks-Mac-mini.local>
2021-09-17 11:59:47 -0600

    Added failing unit test for Crafting Area

--- TrueCraft.Core.Test/CoreSetup.cs
+++ TrueCraft.Core.Test/CoreSetup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Xml;
 using NUnit.Framework;
 using TrueCraft.Core.Logic;
 using TrueCraft.Core.Logic.Blocks;
@@ -39,7 +40,31 @@ namespace TrueCraft.Core.Test
 
             public void DiscoverRecipes(IRegisterRecipe repository)
             {
-                throw new NotImplementedException();
+                XmlDocument doc= new XmlDocument();
+                doc.LoadXml(@"<recipe>
+      <pattern>
+        <r>
+          <c>
+            <id>5</id>
+            <count>1</count>
+          </c>
+        </r>
+        <r>
+          <c>
+            <id>5</id>
+            <count>1</count>
+          </c>
+        </r>
+      </pattern>
+      <output>
+        <id>280</id>
+        <count>4</count>
+      </output>
+    </recipe>
+");
+                XmlNode sticks = doc.DocumentElement;
+
+                repository.RegisterRecipe(new CraftingRecipe(sticks));
             }
         }
 
@@ -49,6 +74,7 @@ namespace TrueCraft.Core.Test
             IDiscover discover = new MockDiscover();
             BlockRepository.Init(discover);
             ItemRepository.Init(discover);
+            CraftingRepository.Init(discover);
         }
     }
 }
--- TrueCraft.Core.Test/Windows/CraftingWindowAreaTest.cs
+++ TrueCraft.Core.Test/Windows/CraftingWindowAreaTest.cs
@@ -3,6 +3,9 @@ using TrueCraft.API.Logic;
 using TrueCraft.API.Windows;
 using TrueCraft.API;
 using NUnit.Framework;
+using TrueCraft.Core.Logic;
+using TrueCraft.Core.Logic.Blocks;
+using TrueCraft.Core.Logic.Items;
 using TrueCraft.Core.Windows;
 using Moq;
 
@@ -11,17 +14,49 @@ namespace TrueCraft.Core.Test.Windows
     [TestFixture]
     public class CraftingWindowAreaTest
     {
-        [Test]
-        public void TestCraftingWindowArea()
-        {
-            var recipe = new Mock<ICraftingRecipe>();
-            recipe.Setup(r => r.Output).Returns(new ItemStack(10));
-            var repository = new Mock<ICraftingRepository>();
-            repository.Setup(r => r.GetRecipe(It.IsAny<CraftingPattern>())).Returns(recipe.Object);
+        // NOTE: These tests depend upon an initialized CraftingRepository
+        //  containing the recipe for sticks.
 
-            var area = new CraftingWindowContent(repository.Object, 2, 2);
-            area[0] = new ItemStack(11);
-            Assert.AreEqual(new ItemStack(10), area[CraftingWindowContent.CraftingOutput]);
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        public void ctor(int width, int height)
+        {
+            CraftingWindowContent actual = new CraftingWindowContent(CraftingRepository.Get(), width, height);
+
+            Assert.AreEqual(1 + width * height, actual.Count);
+            Assert.AreEqual(width, actual.Width);
+            Assert.AreEqual(height, actual.Height);
+        }
+
+        [Test]
+        public void Not_A_Recipe_2x2()
+        {
+            CraftingWindowContent area = new CraftingWindowContent(CraftingRepository.Get(), 2, 2);
+
+            area[1] = new ItemStack(CobblestoneBlock.BlockID);
+
+            Assert.True(area[0].Empty);
+            Assert.False(area[1].Empty);
+            Assert.True(area[2].Empty);
+            Assert.True(area[3].Empty);
+            Assert.True(area[4].Empty);
+        }
+
+        [Test]
+        public void Is_A_Recipe_2x2()
+        {
+            CraftingWindowContent area = new CraftingWindowContent(CraftingRepository.Get(), 2, 2);
+            ItemStack planks = new ItemStack(WoodenPlanksBlock.BlockID);
+
+            area[1] = planks;
+            area[3] = planks;
+
+            Assert.AreEqual(StickItem.ItemID, area[0].ID);
+            Assert.AreEqual(4, area[0].Count);
+            Assert.False(area[1].Empty);
+            Assert.True(area[2].Empty);
+            Assert.False(area[3].Empty);
+            Assert.True(area[4].Empty);
         }
     }
 }
