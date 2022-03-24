b3b09b99ef1d6ae884820a1f9eab4c75d44767fd
mj <mj@Marks-Mac-mini.local>
2022-03-24 14:44:08 -0600

    WIP: Removal of redundant and never-used property

--- TrueCraft.Core/World/ChunkLoadedEventArgs.cs
+++ TrueCraft.Core/World/ChunkLoadedEventArgs.cs
@@ -4,7 +4,6 @@ namespace TrueCraft.Core.World
 {
     public class ChunkLoadedEventArgs : EventArgs
     {
-        public GlobalChunkCoordinates Coordinates { get => Chunk.Coordinates; }
         public IChunk Chunk { get; }
 
         public ChunkLoadedEventArgs(IChunk chunk)
