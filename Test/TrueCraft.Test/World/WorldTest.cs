6521db181fb04bff61330fa37b62d51ff6bff402
mj <mj@Marks-Mac-mini.local>
2022-05-17 08:24:04 -0600

    Fix: added missing Mock so test passes

--- Test/TrueCraft.Test/World/WorldTest.cs
+++ Test/TrueCraft.Test/World/WorldTest.cs
@@ -20,9 +20,12 @@ namespace TrueCraft.Test.World
 
             Mock<IBlockRepository> mockBlockRepository = new Mock<IBlockRepository>(MockBehavior.Strict);
 
+            Mock<IItemRepository> mockItemRepository = new Mock<IItemRepository>(MockBehavior.Strict);
+
             Mock<IServiceLocator> mockServiceLocator = new Mock<IServiceLocator>(MockBehavior.Strict);
             mockServiceLocator.Setup(x => x.Server).Returns(mockServer.Object);
             mockServiceLocator.Setup(x => x.BlockRepository).Returns(mockBlockRepository.Object);
+            mockServiceLocator.Setup(x => x.ItemRepository).Returns(mockItemRepository.Object);
 
             string assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
             IWorld world = TrueCraft.World.World.LoadWorld(mockServiceLocator.Object, Path.Combine(assemblyDir, "Files"));
