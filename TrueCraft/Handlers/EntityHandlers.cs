74f880c8f77e30aa409568f94ac18b309ffb5f36
mj <mj@Marks-Mac-mini.local>
2022-06-28 11:13:02 -0600

    Suppressing nullability warnings in EntityHandlers.
    By the time the client is sending packets regarding player actions, we can expect
    that the player's Entity has been initialized.

--- TrueCraft/Handlers/EntityHandlers.cs
+++ TrueCraft/Handlers/EntityHandlers.cs
@@ -11,13 +11,13 @@ namespace TrueCraft.Handlers
         public static void HandlePlayerPositionPacket(IPacket _packet, IRemoteClient _client, IMultiplayerServer server)
         {
             var packet = (PlayerPositionPacket)_packet;
-            HandlePlayerMovement(_client, new Vector3(packet.X, packet.Y, packet.Z), _client.Entity.Yaw, _client.Entity.Pitch);
+            HandlePlayerMovement(_client, new Vector3(packet.X, packet.Y, packet.Z), _client.Entity!.Yaw, _client.Entity.Pitch);
         }
 
         public static void HandlePlayerLookPacket(IPacket _packet, IRemoteClient _client, IMultiplayerServer server)
         {
             var packet = (PlayerLookPacket)_packet;
-            HandlePlayerMovement(_client, _client.Entity.Position, packet.Yaw, packet.Pitch);
+            HandlePlayerMovement(_client, _client.Entity!.Position, packet.Yaw, packet.Pitch);
         }
 
         public static void HandlePlayerPositionAndLookPacket(IPacket _packet, IRemoteClient _client, IMultiplayerServer server)
@@ -28,7 +28,7 @@ namespace TrueCraft.Handlers
 
         public static void HandlePlayerMovement(IRemoteClient client, Vector3 position, float yaw, float pitch)
         {
-            if (client.Entity.Position.DistanceTo(position) > 10)
+            if (client.Entity!.Position.DistanceTo(position) > 10)
             {
                 //client.QueuePacket(new DisconnectPacket("Client moved to fast (hacking?) " + client.Entity.Position.DistanceTo(position)));
                 // TODO: Figure out a good way of knowing when the client is going to stop BSing us about its position
