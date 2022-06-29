6bf7b35fb2e4bf41f00742d73f0979a7dc1f863b
mj <mj@Marks-Mac-mini.local>
2022-06-29 11:41:10 -0600

    Fix nullability warning in Paths.Base

--- TrueCraft.Core/Paths.cs
+++ TrueCraft.Core/Paths.cs
@@ -9,31 +9,36 @@ namespace TrueCraft.Core
         {
             get
             {
-                var xdg_config_home = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
-                string config = null;
-                if (xdg_config_home != null)
+                string? xdg_config_home = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
+                string? config = null;
+                if (xdg_config_home is not null)
                 {
                     config = Path.Combine(xdg_config_home, "truecraft");
                     if (Directory.Exists(config))
                         return config;
                 }
+
                 var appdata = Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
                     "truecraft");
                 if (Directory.Exists(appdata))
                     return appdata;
+
                 var userprofile = Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
                     ".truecraft");
                 if (Directory.Exists(userprofile))
                     return userprofile;
+
                 // At this point, there's no existing data to choose from, so use the best option
-                if (config != null)
+                if (config is not null)
                 {
                     Directory.CreateDirectory(config);
                     return config;
                 }
+
                 Directory.CreateDirectory(appdata);
+
                 return appdata;
             }
         }
