97bbd6b2b7bb55dc21cadee07352dc70a1953ed1
mj <mj@Marks-Mac-mini.local>
2022-07-18 16:00:37 -0600

    Update Discover.cs to discover Item Providers via the new XML file

--- TrueCraft.Core/Logic/Discover.cs
+++ TrueCraft.Core/Logic/Discover.cs
@@ -11,8 +11,24 @@ namespace TrueCraft.Core.Logic
 {
     public class Discover : IDiscover
     {
+        private readonly XmlDocument _doc;
+
         public Discover()
         {
+            _doc = new XmlDocument();
+
+            Assembly thisAssembly = this.GetType().Assembly;
+            using (Stream xsd = thisAssembly.GetManifestResourceStream("TrueCraft.Core.Assets.TrueCraft.xsd")!)
+                _doc.Schemas.Add(XmlSchema.Read(xsd, null)!);
+
+            using (Stream sz = thisAssembly.GetManifestResourceStream("TrueCraft.Core.Assets.TrueCraft.xml.gz")!)
+            using (Stream s = new GZipStream(sz, CompressionMode.Decompress))
+            using (XmlReader xmlr = XmlReader.Create(s))
+            {
+                _doc.Load(xmlr);
+                _doc.Validate(null);
+            }
+
         }
 
         public static IServiceLocator DoDiscovery(IDiscover discoverer)
@@ -46,13 +62,12 @@ namespace TrueCraft.Core.Logic
 
         public virtual void DiscoverItemProviders(IRegisterItemProvider repository)
         {
-            var providerTypes = new List<Type>();
-            // TODO: This can only enumerate currently loaded assemblies.
-            //  Thus, it will be unable to discover any extensions/mods.
+            // Register Block Item Providers (as they haven't been refactored yet)
+            List<Type> providerTypes = new List<Type>();
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 foreach (var type in assembly.GetTypes().Where(t =>
-                    typeof(IItemProvider).IsAssignableFrom(t) && !t.IsAbstract))
+                    typeof(IBlockProvider).IsAssignableFrom(t) && !t.IsAbstract))
                 {
                     providerTypes.Add(type);
                 }
@@ -61,31 +76,45 @@ namespace TrueCraft.Core.Logic
             providerTypes.ForEach(t =>
             {
                 IItemProvider? instance = (IItemProvider?)Activator.CreateInstance(t);
-                // TODO: If instance is null, it means the developer forgot to
-                //       include a parameterless constructor.  Log a warning.
                 if (instance is not null)
                     repository.RegisterItemProvider(instance);
             });
+
+            // TODO: add enumeration of other xml files in the same folder as
+            //       this Assembly to discover additional items.
+            Assembly thisAssembly = this.GetType().Assembly;
+            Type typeItemProvider = typeof(ItemProvider);
+            XmlNode truecraft = _doc["truecraft"]!;
+            XmlNode items = truecraft["itemrepository"]!;
+            foreach(XmlNode item in items.ChildNodes)
+            {
+                XmlNode? behavior = item["behavior"];
+                IItemProvider? instance = null;
+                if (behavior is null)
+                {
+                    instance = (IItemProvider?)Activator.CreateInstance(typeItemProvider, new object[] { item });
+                }
+                else
+                {
+                    string typeName = behavior.InnerText;
+                    // TODO: Find Assembly (which will need to be specified in XML).
+                    // TODO: Change First to FirstOrDefault and handle null case.
+                    Type typeBehavior = thisAssembly.ExportedTypes.Where(t => t.FullName == typeName).First();
+                    instance = (IItemProvider?)Activator.CreateInstance(typeBehavior, new object[] { item });
+                }
+                // TODO: If instance is null, it means the developer has done
+                //    something incorrectly.  Log a warning.
+                if (instance is not null)
+                    repository.RegisterItemProvider(instance);
+            }
         }
 
 
         public virtual void DiscoverRecipes(IRegisterRecipe repository)
         {
-            XmlDocument doc = new XmlDocument();
-
-            Assembly thisAssembly = this.GetType().Assembly;
-            using (Stream xsd = thisAssembly.GetManifestResourceStream("TrueCraft.Core.Assets.TrueCraft.xsd")!)
-                doc.Schemas.Add(XmlSchema.Read(xsd, null)!);
-
-            using (Stream sz = thisAssembly.GetManifestResourceStream("TrueCraft.Core.Assets.TrueCraft.xml.gz")!)
-            using (Stream s = new GZipStream(sz, CompressionMode.Decompress))
-            using (XmlReader xmlr = XmlReader.Create(s))
-            {
-                doc.Load(xmlr);
-                doc.Validate(null);
-            }
-
-            XmlNode truecraft = doc.ChildNodes.OfType<XmlNode>().Where<XmlNode>(n => n.LocalName == "truecraft").First<XmlNode>();
+            // TODO: add enumeration of other xml files in the same folder as
+            //       this Assembly to discover additional Crafting Recipes.
+            XmlNode truecraft = _doc.ChildNodes.OfType<XmlNode>().Where<XmlNode>(n => n.LocalName == "truecraft").First<XmlNode>();
             XmlNode recipes = truecraft.ChildNodes.OfType<XmlNode>().Where<XmlNode>(n => n.LocalName == "recipes").First<XmlNode>();
             foreach (XmlNode recipe in recipes.ChildNodes)
                 repository.RegisterRecipe(new CraftingRecipe(recipe));
