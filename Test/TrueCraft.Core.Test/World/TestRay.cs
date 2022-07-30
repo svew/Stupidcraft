182858533bec7fd4a4769c3b19f341c626f79fd9
mj <mj@Marks-Mac-mini.local>
2022-07-30 14:31:21 -0600

    Correct two Intersects unit test; add two more (one failing)

--- Test/TrueCraft.Core.Test/World/TestRay.cs
+++ Test/TrueCraft.Core.Test/World/TestRay.cs
@@ -86,68 +86,6 @@ namespace TrueCraft.Core.Test.World
 
         public static IEnumerable<object[]> IntersectsTestData()
         {
-            //// A simple x-axis-aligned ray that terminates exactly on the
-            //// negative-x surface of the Box.
-            //yield return new object[] {
-            //    true, 1.0, BlockFace.NegativeX,
-            //    new Ray(new Vector3(-1.0, 0.5, 0.5), new Vector3(1, 0, 0)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A simple x-axis-aligned ray that terminates exactly on the
-            //// positive-x surface of the Box.
-            //yield return new object[]
-            //{
-            //    true, 1.0, BlockFace.PositiveX,
-            //    new Ray(new Vector3(2, 0.5, 0.5), new Vector3(-1, 0, 0)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A simple y-axis-aligned ray that terminates exactly on the
-            //// positive-y surface of the Box.
-            //yield return new object[]
-            //{
-            //    true, 1.0, BlockFace.PositiveY,
-            //    new Ray(new Vector3(0.5, 2, 0.5), new Vector3(0, -1, 0)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A simple y-axis-aligned ray that terminates exactly on the
-            //// negative-y surface of the Box.
-            //yield return new object[]
-            //{
-            //    true, 1.0, BlockFace.NegativeY,
-            //    new Ray(new Vector3(0.5, -1, 0.5), new Vector3(0, 1, 0)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A simple z-axis-aligned ray that terminates exactly on the
-            //// positive-z surface of the Box.
-            //yield return new object[]
-            //{
-            //    true, 1.0, BlockFace.PositiveZ,
-            //    new Ray(new Vector3(0.5, 0.5, 2), new Vector3(0, 0, -1)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A simple z-axis-aligned ray that terminates exactly on the
-            //// negative-z surface of the Box.
-            //yield return new object[]
-            //{
-            //    true, 1.0, BlockFace.NegativeZ,
-            //    new Ray(new Vector3(0.5, 0.5, -1), new Vector3(0, 0, 1)),
-            //    new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
-            //};
-
-            //// A Ray that crosses every plane of the Box, but does not
-            //// intersect the box.
-            //yield return new object[]
-            //{
-            //    false, 0, BlockFace.PositiveY,
-            //    new Ray(new Vector3(-5, -5, -5), new Vector3(10, 10, 10)),
-            //    new BoundingBox(new Vector3(3, -3, 3), new Vector3(4, -4, 4))
-            //};
-
             // A ray that crosses no planes of the box, and therefore
             // cannot intersect it.
             yield return new object[]
@@ -155,6 +93,34 @@ namespace TrueCraft.Core.Test.World
                 false, 0, BlockFace.PositiveY,
                 new Ray(new Vector3(-5, -5, -5), new Vector3(10, 10, 10)),
                 new BoundingBox(new Vector3(300, 30, 250), new Vector3(304, 34, 254))
+            };
+
+            // A Ray that is aimed at the negative-X surface of the box,
+            // but stops short of it, and therefore does not intersect it.
+            // t in the range 0..1
+            //  x = 4 * t - 3
+            //  y = -1 * t + 13.2
+            //  z = 2 * t - 0.8
+            // It would intersects the box at t = 2; (x,y,z) = (0.3, 0.05, 1)
+            yield return new object[]
+            {
+                false, double.MinValue, BlockFace.NegativeX,
+                new Ray(new Vector3(-3, 13.2, -0.8), new Vector3(4, -1, 2)),
+                new BoundingBox(new Vector3(5, 11, 3), new Vector3(6, 12, 4))
+            };
+
+            // A Ray that starts outside the positive-X surface and points
+            // away from the Box.  It does NOT intersect the Box.
+            // t in the range 0..1
+            //  x = 5t + 4.5
+            //  y = -4t - 2.5
+            //  z = -3t - 2.05
+            // It would intersects the box at t = -0.7; (x,y,z) = (1, 0.3, 0.05)
+            yield return new object[]
+            {
+                false, double.MinValue, BlockFace.PositiveX,
+                new Ray(new Vector3(4.5, -2.5, -2.05), new Vector3(5, -4, -3)),
+                new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1))
             };
 
             // A ray that enters the negative-X surface of the box at an angle.
@@ -277,14 +243,14 @@ namespace TrueCraft.Core.Test.World
             {
                 true, 0.0, BlockFace.NegativeZ,
                 new Ray(new Vector3(3217.6, 64.5, 497), new Vector3(0, 0, -5)),
-                new BoundingBox(new Vector3(3217, 64, 497), new Vector3(3218, 64, 497))
+                new BoundingBox(new Vector3(3217, 64, 497), new Vector3(3218, 65, 497))
             };
 
             // This Ray starts on the Positive-Z surface and points away.
             yield return new object[]
             {
                 true, 0.0, BlockFace.PositiveZ,
-                new Ray(new Vector3(217.4, 63, -354), new Vector3(0.02, -.3, 3)),
+                new Ray(new Vector3(217.4, 63.75, -354), new Vector3(0.02, -.3, 3)),
                 new BoundingBox(new Vector3(217, 63, -354), new Vector3(281, 64, -354))
             };
         }
@@ -306,6 +272,28 @@ namespace TrueCraft.Core.Test.World
             else
             {
                 Assert.False(actualDistance.HasValue);
+            }
+        }
+
+        [TestCaseSource(nameof(IntersectsTestData))]
+        public void IntersectsNew(bool expectedToIntersect, double expectedDistance, BlockFace expectedBlockFace,
+            Ray ray, BoundingBox box)
+        {
+            BlockFace actualBlockFace = BlockFace.PositiveY;
+            double actualDistance = double.MinValue;
+
+            bool actualIntersects = ray.Intersects(box, ref actualDistance, ref actualBlockFace);
+
+            if (expectedToIntersect)
+            {
+                Assert.True(actualIntersects);
+                Assert.True(Math.Abs(expectedDistance - actualDistance) < GameConstants.Epsilon,
+                    "Expected Distance: {0}\nActual Distance: {1}", expectedDistance, actualDistance);
+                Assert.AreEqual(expectedBlockFace, actualBlockFace);
+            }
+            else
+            {
+                Assert.False(actualIntersects);
             }
         }
     }
